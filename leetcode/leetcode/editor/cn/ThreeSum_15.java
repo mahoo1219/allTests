60c4e37ac8bd6297a695179fab2d354c8b03f550
mahoo1219 <mahoo1219@gmail.com>
2022-08-09 23:13:18 +0800

    15

--- leetcode/leetcode/editor/cn/ThreeSum_15.java
+++ leetcode/leetcode/editor/cn/ThreeSum_15.java
@@ -44,20 +44,58 @@ package cn;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ThreeSum_15 {
     @Test
     public void runTest() {
         Solution solution = new ThreeSum_15().new Solution();
-        Assert.assertEquals("[[-1,-1,2],[-1,0,1]]", RunTools.int2dListToString(solution.threeSum(RunTools.stringToIntegerArray("[-1,0,1,2,-1,-4]"))));
+        Assert.assertEquals("[[-2, 0, 2],[-2, 1, 1]]", RunTools.int2dListToString(solution.threeSum(RunTools.stringToIntegerArray("[-2,0,1,1,2]"))));
+        Assert.assertEquals("[[-1, -1, 2],[-1, 0, 1]]", RunTools.int2dListToString(solution.threeSum(RunTools.stringToIntegerArray("[-1,0,1,2,-1,-4]"))));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
-            return Collections.emptyList();
+            if (nums == null || nums.length == 0) {
+                return Collections.emptyList();
+            }
+
+            List<List<Integer>> ans = new LinkedList<>();
+            int n = nums.length;
+            Arrays.sort(nums);
+
+            for (int first = 0; first < n; first++) {
+                if (first != 0 && nums[first] == nums[first - 1]) {
+                    // same first num
+                    continue;
+                }
+
+                int second = first + 1, third = n - 1;
+                while (second < third) {
+                    int val = nums[first] + nums[second] + nums[third];
+                    if (val > 0) {
+                        third--;
+                    } else if (val < 0) {
+                        second++;
+                    } else {
+                        List<Integer> temp = Arrays.asList(nums[first], nums[second], nums[third]);
+                        ans.add(temp);
+                        second++;
+                        third--;
+                        while (second < third && nums[second] == nums[second - 1]) {
+                            second++;
+                        }
+                        while (second < third && nums[third] == nums[third + 1]) {
+                            third--;
+                        }
+                    }
+                }
+            }
+            return ans;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
