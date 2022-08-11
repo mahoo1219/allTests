fe2e2d81886d788820378ed0a374e4883d7893a7
mahoo1219 <mahoo1219@gmail.com>
2022-08-11 23:12:22 +0800

    runtools

--- leetcode/leetcode/editor/cn/tools/RunTools.java
+++ leetcode/leetcode/editor/cn/tools/RunTools.java
@@ -1,8 +1,221 @@
 package cn.tools;
 
-import java.util.List;
+import com.eclipsesource.json.JsonArray;
+
+import java.util.*;
 
 public class RunTools {
+    /**
+     * 10^9 + 7
+     */
+    public static final int mod = 1000000007;
+
+    public void printIndent(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("  ");
+        }
+    }
+
+    private static List<Integer> stringToIntegerList(String input) {
+        JsonArray jsonArray = JsonArray.readFrom(input);
+        List<Integer> arr = new ArrayList<>(jsonArray.size());
+        for (int i = 0; i < jsonArray.size(); i++) {
+            arr.add(jsonArray.get(i).asInt());
+        }
+        return arr;
+    }
+
+    public static List<List<Integer>> stringToInt2dList(String input) {
+        JsonArray jsonArray = JsonArray.readFrom(input);
+        if (jsonArray.size() == 0) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> list = new ArrayList<>(jsonArray.size());
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JsonArray cols = jsonArray.get(i).asArray();
+            list.add(stringToIntegerList(cols.toString()));
+        }
+        return list;
+    }
+
+    public static int[][] stringToInt2dArray(String input) {
+        JsonArray jsonArray = JsonArray.readFrom(input);
+        if (jsonArray.size() == 0) {
+            return new int[0][0];
+        }
+
+        int[][] arr = new int[jsonArray.size()][];
+        for (int i = 0; i < arr.length; i++) {
+            JsonArray cols = jsonArray.get(i).asArray();
+            arr[i] = stringToIntegerArray(cols.toString());
+        }
+        return arr;
+    }
+
+
+    public static String intArrayToString(int[] arr) {
+        return Arrays.toString(arr);
+    }
+
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (node != null) {
+            result.append(node.val).append(", ");
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+
+        StringBuilder output = new StringBuilder();
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (node == null) {
+                output.append("null, ");
+                continue;
+            }
+
+            output.append(node.val).append(", ");
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+
+    public static Node stringToNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        Node root = new Node(Integer.parseInt(item));
+        Queue<Node> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while (!nodeQueue.isEmpty()) {
+            Node node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new Node(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new Node(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String nodeToString(Node root) {
+        if (root == null) {
+            return "[]";
+        }
+
+        StringBuilder output = new StringBuilder();
+        Queue<Node> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while (!nodeQueue.isEmpty()) {
+
+            Node node = nodeQueue.remove();
+
+            if (node == null) {
+                output.append("null, ");
+                continue;
+            }
+
+            output.append(node.val).append(", ");
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+
+    public static int getLength(ListNode head) {
+        int cnt = 0;
+        while (head != null) {
+            cnt++;
+            head = head.next;
+        }
+        return cnt;
+    }
+
+
     public static int[] stringToIntegerArray(String input) {
         input = input.trim();
         input = input.substring(1, input.length() - 1);
@@ -24,10 +237,10 @@ public class RunTools {
             return "[]";
         }
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int index = 0; index < length; index++) {
             Integer number = nums.get(index);
-            result += number + ", ";
+            result.append(number).append(", ");
         }
         return "[" + result.substring(0, result.length() - 2) + "]";
     }
@@ -54,7 +267,7 @@ public class RunTools {
         // Now convert that list into linked list
         ListNode dummyRoot = new ListNode(0);
         ListNode ptr = dummyRoot;
-        for(int item : nodeValues) {
+        for (int item : nodeValues) {
             ptr.next = new ListNode(item);
             ptr = ptr.next;
         }
